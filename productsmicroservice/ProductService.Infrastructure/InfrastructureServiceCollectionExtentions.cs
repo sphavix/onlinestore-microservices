491c82303f33ff57e6d56eaa949ee72c1be541b9
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2025-06-12 14:46:28 +0200

    Containerizing microservices with Docker Compose

--- productsmicroservice/ProductService.Infrastructure/InfrastructureServiceCollectionExtentions.cs
+++ productsmicroservice/ProductService.Infrastructure/InfrastructureServiceCollectionExtentions.cs
@@ -11,8 +11,14 @@ public static class InfrastructureServiceCollectionExtentions
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Register your infrastructure services here
+        //string connectionStringTemplate = configuration.GetConnectionString("DefaultConnection")!;
+
+        //string connectionString = connectionStringTemplate.Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST"))
+        //    .Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD"));
+
         services.AddDbContext<ApplicationDbContext>(options =>
         {
+            //options.UseMySQL(connectionString);
             options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
         });
 
